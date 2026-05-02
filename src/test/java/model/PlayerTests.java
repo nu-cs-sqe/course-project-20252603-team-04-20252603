e60fcd967f87869abb0b563ce3c972c956908652
Omotayo Oludemi <omotayooludemi@gmail.com>
2026-05-02 18:51:06 -0500

    rename test method for buying with valid money; update test to check buying with slightly less than balance

--- src/test/java/model/PlayerTests.java
+++ src/test/java/model/PlayerTests.java
@@ -52,9 +52,9 @@ public class PlayerTests {
 
     @Test
 
-    public void Tests_Buying_With_Valid_Money() {
+    public void Tests_Buying_With_Slightl_Less_Than_Balance() {
         Player player = new Player("John", 100.0);
-        boolean success = player.buy(50.0);
+        boolean success = player.buy(99.0);
 
         assertTrue(success, "Buying with valid amount should be successful");
         assertEquals(50.0, player.getBalance(), 0.001, "Balance should decrease by the purchase amount");
@@ -64,14 +64,6 @@ public class PlayerTests {
     public void Tests__Buying_With_Max_Integer() {
         Player player = new Player("John", 100.0);
         boolean success = player.buy(Double.MAX_VALUE);
-
-        assertFalse(success, "Buying with maximum amount should be rejected/properly handled");
-        assertEquals(100.0, player.getBalance(), 0.001, "Balance should not change");
-    }
-
-    public void Tests__Buying_With_Min_Integer() {
-        Player player = new Player("John", 100.0);
-        boolean success = player.buy(Double.MIN_VALUE);
 
         assertFalse(success, "Buying with maximum amount should be rejected/properly handled");
         assertEquals(100.0, player.getBalance(), 0.001, "Balance should not change");
