b7050cffe58cb857b041d6b7ba83eec0f43dadea
maxtong2023 <maxtong2027@u.northwestern.edu>
2026-05-02 20:13:19 -0500

    wrote failing test exception on start game with 5 players

--- src/test/java/model/GameEngineTest.java
+++ src/test/java/model/GameEngineTest.java
@@ -56,4 +56,20 @@ public class GameEngineTest {
         EasyMock.verify(player1, player2, player3, player4);
         
     }
+
+    @Test
+    public void Start_Game_With_Five_Players_Throws_Exception(){
+        Player player1 = EasyMock.createMock(Player.class);
+        Player player2 = EasyMock.createMock(Player.class);
+        Player player3 = EasyMock.createMock(Player.class);
+        Player player4 = EasyMock.createMock(Player.class);
+        Player player5 = EasyMock.createMock(Player.class);
+
+        EasyMock.replay(player1, player2, player3, player4, player5);
+        
+        GameEngine gameEngine = new GameEngine(List.of(player1, player2, player3, player4, player5));
+        assertThrows(IllegalArgumentException.class, () -> gameEngine.startGame());
+
+        EasyMock.verify(player1, player2, player3, player4, player5);
+    }
 }
