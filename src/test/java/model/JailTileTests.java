d692a4cd1c10ad0a490f026a5809c9a4b2f3e5bf
jskpaul <paulkim2025@u.northwestern.edu>
2026-05-14 14:49:18 -0500

    Test_JailTile_LandOn_NotInJail passes with bare minimum landOn code

--- src/test/java/model/JailTileTests.java
+++ src/test/java/model/JailTileTests.java
@@ -1,6 +1,9 @@
 package model;
 
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class JailTileTests {
 
@@ -8,5 +11,18 @@ public class JailTileTests {
     public void Test_JailTile_Name(){
         JailTile jailTile = new JailTile();
         assert(jailTile.getName() == TileType.JAIL);
+    }
+
+    @Test
+    public void Test_JailTile_LandOn_NotInJail(){
+        JailTile jailTile = new JailTile();
+        Player player = EasyMock.createMock(Player.class);
+        GameEngine game = EasyMock.createMock(GameEngine.class);
+        EasyMock.expect(player.isActive()).andStubReturn(true);
+        EasyMock.expect(player.inJail()).andReturn(false);
+        EasyMock.replay(player, game);
+        jailTile.landOn(player, game);
+        assertFalse(player.inJail());
+        EasyMock.verify(player, game);
     }
 }
