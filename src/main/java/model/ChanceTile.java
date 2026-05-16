d6d4dfad856d1653f5f6825a8b9cf5fe06ae0ea4
maxtong2023 <maxtong2027@u.northwestern.edu>
2026-05-15 19:31:43 -0500

    added TC1 and minimum working implementation to pass

---
+++ src/main/java/model/ChanceTile.java
@@ -0,0 +1,15 @@
+package model;
+
+public class ChanceTile implements Tile {
+
+    @Override
+    public TileType getName() {
+        return TileType.CHANCE;
+    }
+
+    @Override
+    public void landOn(Player player, GameEngine game){
+        
+    }
+
+}
---
+++ src/test/java/model/ChanceTileTests.java
@@ -0,0 +1,17 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ChanceTileTests {
+
+    //TC1: ChanceTile reports its tile type
+
+    @Test
+    public void Tests_ChanceTile_Reports_Its_Tile_Type(){
+        ChanceTile chanceTile = new ChanceTile();
+        assertEquals(TileType.CHANCE, chanceTile.getName());
+    }
+
+}
