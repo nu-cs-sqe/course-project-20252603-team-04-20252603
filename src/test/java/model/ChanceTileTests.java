4f3123122ad60cb3de94506809ad6fdb21440a7c
maxtong2023 <maxtong2027@u.northwestern.edu>
2026-05-18 11:35:08 -0500

    added failing test for active player lands on chance tile

--- src/test/java/model/ChanceTileTests.java
+++ src/test/java/model/ChanceTileTests.java
@@ -2,11 +2,10 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
 public class ChanceTileTests {
-
-    //TC1: ChanceTile reports its tile type
 
     @Test
     public void Tests_ChanceTile_Reports_Its_Tile_Type() {
@@ -14,4 +13,24 @@ public class ChanceTileTests {
         assertEquals(TileType.CHANCE, chanceTile.getName());
     }
 
+    // TC2: Active player lands on ChanceTile
+
+    @Test
+    public void Active_Player_Lands_On_ChanceTile() {
+        Player player = EasyMock.createMock(Player.class);
+        GameEngine game = EasyMock.createMock(GameEngine.class);
+        Deck deck = EasyMock.createMock(Deck.class);
+
+        EasyMock.expect(player.getActive()).andReturn(true);
+        EasyMock.expect(game.getChanceDeck()).andReturn(deck);
+        deck.draw();
+        EasyMock.expectLastCall();
+
+        EasyMock.replay(player, game, deck);
+
+        ChanceTile chanceTile = new ChanceTile();
+        chanceTile.landOn(player, game);
+
+        EasyMock.verify(player, game, deck);
+    }
 }
