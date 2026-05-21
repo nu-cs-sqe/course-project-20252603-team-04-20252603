1092c85203640a3b1200a5c36a5216aed77ebc4f
Omotayo Oludemi <omotayooludemi@gmail.com>
2026-05-21 15:10:19 -0500

    Add additional test for FreeParking getName method to ensure it is not null

--- src/test/java/model/FreeParkingTests.java
+++ src/test/java/model/FreeParkingTests.java
@@ -16,5 +16,18 @@ public class FreeParkingTests {
         assertEquals(TileType.FREE, tile.getName(), "FreeParking should report TileType.FREE");
     }
 
+
+    @Test 
+    public void TC2_GetName_Is_Not_Null() {
+        FreeParking tile = new FreeParking();
+        assertEquals(false, tile.getName() == null, "FreeParking name should never be null");
+    }
+
+    
+
+
+    
+
+
     
 }
