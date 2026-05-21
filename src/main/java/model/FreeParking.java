263b8674e7f7ef38f36648c6435d1ebd8f7415ea
Omotayo Oludemi <omotayooludemi@gmail.com>
2026-05-21 15:01:51 -0500

    Implement FreeParking class with getName and landOn methods

---
+++ src/main/java/model/FreeParking.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Objects;
+
+public class FreeParking implements Tile {
+
+
+    @Override
+    public TileType getName() {
+        return TileType.FREE;
+    }
+
+  
+
+
+}
