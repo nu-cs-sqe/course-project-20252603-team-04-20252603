aaaf42dead56672922ecc9a61605baed4cce2dca
maxtong2023 <maxtong2027@u.northwestern.edu>
2026-05-23 13:13:37 -0500

    added TC7, passes with current implementation

--- src/test/java/model/DeckTests.java
+++ src/test/java/model/DeckTests.java
@@ -151,6 +151,39 @@ public class DeckTests {
     }
 
     @Test
+    public void TC7_Shuffle_FourCards() {
+        Card c1 = EasyMock.createMock(Card.class);
+        Card c2 = EasyMock.createMock(Card.class);
+        Card c3 = EasyMock.createMock(Card.class);
+        Card c4 = EasyMock.createMock(Card.class);
+
+        Random rand = EasyMock.createMock(Random.class);
+        EasyMock.expect(rand.nextInt(4)).andReturn(0);
+        EasyMock.expect(rand.nextInt(3)).andReturn(0);
+        EasyMock.expect(rand.nextInt(2)).andReturn(0);
+        EasyMock.replay(rand);
+
+        Deck deck = new Deck(rand);
+        deck.getUnusedCards().add(c1);
+        deck.getUnusedCards().add(c2);
+        deck.getUnusedCards().add(c3);
+        deck.getUnusedCards().add(c4);
+        deck.shuffle();
+
+        assertSame(c2, deck.getUnusedCards().removeFirst(),
+                "front of deque should be C2 after shuffle");
+        assertSame(c3, deck.getUnusedCards().removeFirst(),
+                "second card in deque should be C3 after shuffle");
+        assertSame(c4, deck.getUnusedCards().removeFirst(),
+                "third card in deque should be C4 after shuffle");
+        assertSame(c1, deck.getUnusedCards().removeFirst(),
+                "fourth card in deque should be C1 after shuffle");
+        assertTrue(deck.getUsedCards().isEmpty(),
+                "usedCards should remain empty after shuffle");
+        EasyMock.verify(rand);
+    }
+
+    @Test
     public void TC9_Draw_WhenUnusedPileHasMultipleCards() {
         Card c1 = EasyMock.createMock(Card.class);
         Card c2 = EasyMock.createMock(Card.class);
