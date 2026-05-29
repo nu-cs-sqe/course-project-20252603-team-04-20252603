59cddf4a5048bc642f01fae0719f14b60c9542e8
Faris Eltayib <chefredr@gmail.com>
2026-05-28 20:55:28 -0500

    Fixed instances of magic numbers

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -3,6 +3,7 @@ package util;
 public class Constants {
 
     public static final double STARTING_BALANCE = 1000.0;
+    public static final int MAX_NUM_PLAYERS = 4;
     public static final int JAIL_POSITION = 10;
     public static final int BOARD_SIZE = 32;
     public static final double SELL_MULTIPLIER = 0.8;
