65f0899a98f0445587233a31c194fb8dee4dc5b2
jskpaul <paulkim2025@u.northwestern.edu>
2026-06-07 22:58:15 -0500

    TC12_startGame_WithEmptyPlayerList_ThrowsException updated, checks that game engine status reamins at not started

--- src/test/java/controller/GameControllerTests.java
+++ src/test/java/controller/GameControllerTests.java
@@ -188,8 +188,12 @@ public class GameControllerTests {
         PlayerInfoView playerInfoView = EasyMock.createMock(PlayerInfoView.class);
         DiceView diceView = EasyMock.createMock(DiceView.class);
         CardView cardView = EasyMock.createMock(CardView.class);
+        EasyMock.expect(gameEngine.getStatus()).andReturn(GameStatus.NOT_STARTED);
+        EasyMock.replay(gameEngine);
         GameController gameController = new GameController(gameEngine, boardView, playerInfoView, diceView, cardView);
         assertThrows(IllegalArgumentException.class, () -> gameController.startGame(List.of()));
+        assertEquals(GameStatus.NOT_STARTED, gameController.getStatus());
+        EasyMock.verify(gameEngine);
     }
 
     @Test
