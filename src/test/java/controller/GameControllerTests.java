390b0388e0708cfae98f1ab145d2e7e07009eaf4
jskpaul <paulkim2025@u.northwestern.edu>
2026-06-08 04:14:15 -0500

    TC34_handleEndTurn_WhenNextPlayerIsBankrupt_SkipsRemovedPlayer passes

--- src/test/java/controller/GameControllerTests.java
+++ src/test/java/controller/GameControllerTests.java
@@ -865,6 +865,43 @@ public class GameControllerTests {
         EasyMock.verify(gameEngine, boardView, playerInfoView, diceView, cardView, dice);
     }
 
+    @Test
+    public void TC34_handleEndTurn_WhenNextPlayerIsBankrupt_SkipsRemovedPlayer() {
+        GameEngine gameEngine = EasyMock.createMock(GameEngine.class);
+        BoardView boardView = EasyMock.createMock(BoardView.class);
+        PlayerInfoView playerInfoView = EasyMock.createMock(PlayerInfoView.class);
+        DiceView diceView = EasyMock.createMock(DiceView.class);
+        CardView cardView = EasyMock.createMock(CardView.class);
+        Dice dice = EasyMock.createMock(Dice.class);
+
+        EasyMock.expect(gameEngine.isGameOver()).andReturn(false);
+
+        gameEngine.nextTurn();
+        EasyMock.expectLastCall().once();
+
+        boardView.refresh(gameEngine);
+        EasyMock.expectLastCall().once();
+
+        playerInfoView.refresh(gameEngine);
+        EasyMock.expectLastCall().once();
+
+        diceView.refresh(dice);
+        EasyMock.expectLastCall().once();
+
+        cardView.refresh(gameEngine);
+        EasyMock.expectLastCall().once();
+
+        EasyMock.replay(gameEngine, boardView, playerInfoView, diceView, cardView, dice);
+
+        GameController gameController = new GameController(
+                gameEngine, boardView, playerInfoView, diceView, cardView, dice
+        );
+
+        gameController.handleEndTurn();
+
+        EasyMock.verify(gameEngine, boardView, playerInfoView, diceView, cardView, dice);
+    }
+
 
 
     @Test
