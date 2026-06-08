4d13c6a10974b6b2e538ad38b8705a23e6431b05
jskpaul <paulkim2025@u.northwestern.edu>
2026-06-07 23:28:02 -0500

    TC15_startGame_WithMaximumPlayers_StartsGame passes

--- src/test/java/controller/GameControllerTests.java
+++ src/test/java/controller/GameControllerTests.java
@@ -237,6 +237,29 @@ public class GameControllerTests {
         EasyMock.verify(gameEngine);
     }
 
-
+    @Test
+    public void TC15_startGame_WithMaximumPlayers_StartsGame() {
+        GameEngine gameEngine = EasyMock.createMock(GameEngine.class);
+        BoardView boardView = EasyMock.createMock(BoardView.class);
+        PlayerInfoView playerInfoView = EasyMock.createMock(PlayerInfoView.class);
+        DiceView diceView = EasyMock.createMock(DiceView.class);
+        CardView cardView = EasyMock.createMock(CardView.class);
+        Player player1 = EasyMock.createMock(Player.class);
+        Player player2 = EasyMock.createMock(Player.class);
+        Player player3 = EasyMock.createMock(Player.class);
+        Player player4 = EasyMock.createMock(Player.class);
+        EasyMock.expect(gameEngine.getStatus()).andReturn(GameStatus.IN_PROGRESS);
+        EasyMock.expect(gameEngine.getActivePlayers()).andReturn(List.of(player1, player2, player3, player4));
+        EasyMock.expect(gameEngine.getCurrentPlayer()).andReturn(player1);
+        gameEngine.startGame();
+        EasyMock.expectLastCall().once();
+        EasyMock.replay(gameEngine);
+        GameController gameController = new GameController(gameEngine, boardView, playerInfoView, diceView, cardView);
+        gameController.startGame(List.of(player1, player2, player3, player4));
+        assertEquals(GameStatus.IN_PROGRESS, gameController.getStatus());
+        assertEquals(List.of(player1, player2, player3, player4), gameController.getActivePlayers());
+        assertSame(player1, gameController.getCurrentPlayer());
+        EasyMock.verify(gameEngine);
+    }
 
 }
