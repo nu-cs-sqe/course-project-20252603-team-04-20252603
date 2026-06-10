ce40d8ea64ef3068b51f92c72263051911ae3dc6
Faris Eltayib <chefredr@gmail.com>
2026-06-09 19:51:51 -0500

    Added private function to test suites to have a non-null image icon

--- src/test/java/controller/MainMenuControllerIntegrationTests.java
+++ src/test/java/controller/MainMenuControllerIntegrationTests.java
@@ -5,16 +5,21 @@ import model.GameStatus;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
+import java.awt.image.BufferedImage;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainMenuControllerIntegrationTests {
 
+    private ImageIcon createIcon() {
+        return new ImageIcon(new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB));
+    }
+
     @Test
     public void startNewGame_WithValidSetup_SetsGameStatusToInProgress() {
         List<String> playerNames = List.of("John", "Jane");
-        List<ImageIcon> playerIcons = List.of(new ImageIcon(), new ImageIcon());
+        List<ImageIcon> playerIcons = List.of(createIcon(), createIcon());
         MainMenuController controller = new MainMenuController(playerNames, playerIcons);
 
         GameEngine game = controller.startNewGame();
@@ -28,7 +33,7 @@ public class MainMenuControllerIntegrationTests {
     @Test
     public void startNewGame_WithValidSetup_SetsFirstPlayerAsCurrentPlayer() {
         List<String> playerNames = List.of("John", "Jane");
-        List<ImageIcon> playerIcons = List.of(new ImageIcon(), new ImageIcon());
+        List<ImageIcon> playerIcons = List.of(createIcon(), createIcon());
         MainMenuController controller = new MainMenuController(playerNames, playerIcons);
 
         GameEngine game = controller.startNewGame();
